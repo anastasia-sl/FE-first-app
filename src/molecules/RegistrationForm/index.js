0f02acfb7dd846d374dd8bf4d78158b12cfa4566
anastasia-sl <anastasiaslynko2206@gmail.com>
2023-10-01 16:03:37 +0300

    Registration form, request to the server

--- src/molecules/RegistrationForm/index.js
+++ src/molecules/RegistrationForm/index.js
@@ -4,53 +4,95 @@ import './style.scss';
 import Typography from "../../atoms/Typography";
 import Button from "../../atoms/Button";
 import {Link} from "react-router-dom";
+import axios from 'axios';
 
-function RegistrationForm (props){
-    const [value, setValue] = useState();
-    const {username, email, password} = props;
+function RegistrationForm() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState(null);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios.post('/api/v1/auth/register', formData)
+            .then((response) => {
+                setResponse({
+                    username: response.data.username,
+                    jwtToken: response.data.jwtToken,
+                });
+                setError(null);
+                console.log("Response from the server:");
+                console.log("String username:", response.data.username);
+                console.log("String jwtToken(to be changed in upcoming tickets):", response.data.jwtToken);
+                console.log(formData);
+            })
+            .catch((error) => {
+                setResponse(null);
+                setError(error.message);
+            });
+
+        // axios.post('/api/v1/auth/register', formData)
+        //     .then((response) => {
+        //         setResponse(response.data);
+        //         setError(null);
+        //         console.log(formData)
+        //     })
+        //     .catch((error) => {
+        //         setResponse(null);
+        //         setError(error.message);
+        //     });
+    }
 
     return (
-        <div className='Form'>
-            <div className='FormDiv'>
+        <div>
+            <form className='FormBlock' onSubmit={handleSubmit}>
                 <Typography fontWeight='body3' fontSize='title2'>Registration</Typography>
-                <label style={{ color: 'white', fontSize: '20px', fontWeight: '400'}}>Name:</label>
-                <input
-                    value={username}
-                    name="name"
-                    type="text"
-                    onChange={(e) => {setValue(e.target.value)}}
-                />
-                <label style={{ color: 'white', fontSize: '20px', fontWeight: '400'}}>Password:</label>
-                <input
-                    value={password}
-                    name="password"
-                    type="text"
-                    onChange={(e) => {setValue(e.target.value)}}
-                />
-                <label style={{ color: 'white', fontSize: '20px', fontWeight: '400'}}>Email:</label>
-                <input
-                    value={email}
-                    name="email"
-                    type="text"
-                    onChange={(e) => {setValue(e.target.value)}}
-                />
+                    <label style={{ color: 'white', fontSize: '20px', fontWeight: '400'}}>Name</label>
+                    <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                    />
+                    <label style={{ color: 'white', fontSize: '20px', fontWeight: '400'}}>Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                    <label style={{ color: 'white', fontSize: '20px', fontWeight: '400'}}>Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                {/*<button type="submit">Зарегистрироваться</button>*/}
                 <div className="RegButtonDiv">
-                        <Link to="/home" className="GoBackLink">
-                            <Button title='Back' size='big' borderRadius='small' />
-                        </Link>
-                    <Button title='Sign up' size='big' borderRadius='small' />
+                     <Link to="/home" className="GoBackLink">
+                         <Button title='Back' size='big' borderRadius='small' />
+                    </Link>
+                    <Button onSubmit={handleSubmit} title='Sign up' size='big' borderRadius='small' />
                 </div>
-            </div>
+            </form>
+            {response && <div>Регистрация успешна: {response}</div>}
+            {error && <div>Ошибка: {error}</div>}
         </div>
-    )
+    );
 }
-
-RegistrationForm.propTypes = {
-
-}
-RegistrationForm.defaultProps = {
-
-}
-
 
 export default RegistrationForm;
