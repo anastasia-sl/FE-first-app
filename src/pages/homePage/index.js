75d51c2f05c01f18a721f8e7eeb5d931115db2aa
anastasia-sl <anastasiaslynko2206@gmail.com>
2023-11-27 23:30:51 +0200

    home page styles

--- src/pages/homePage/index.js
+++ src/pages/homePage/index.js
@@ -1,30 +1,22 @@
 import './style.scss';
 import {Link} from "react-router-dom";
-import Button from "../../atoms/Button";
 import React, {useState} from "react";
-import FeedbackModal from "../../molecules/FeedbackModal";
 import { observer } from 'mobx-react-lite';
-import userStore from "../../store";
+import WSLogo from '../../atoms/icons/WS_logo.png'
 
 function HomePage() {
     const [modalActive, setModalActive] = useState(false);
 
 
     return (
-        <div>
-            {/*<h1>Home</h1>*/}
-            <div>
-                {/*{!userStore.isLogin && (*/}
-                {/*    <>*/}
-                        <Link to="/registration">Registration</Link>
-                        <Link to="/logIn">LogIn</Link>
-                {/*    </>*/}
-                {/*    )*/}
-                {/*}*/}
-                {/*<Button title='Leave your feedback' onClick={() => setModalActive(true)} backgrndColor='violet' borderRadius='small' hover='true' size='medium' textColor='white'/>*/}
-                {/*<FeedbackModal active={modalActive} setActive={setModalActive}/>*/}
-
-            </div>
+        <div className='HomePage'>
+            <header className='HomePageHeader'>
+                <img className='WSLogo' src={WSLogo}/>
+                <div className='LoginReg'>
+                    <Link to="/logIn" className='LoginRegLink'>Log in</Link>
+                    <Link to="/registration" className='LoginRegLink'>Sign up</Link>
+                </div>
+            </header>
         </div>
     );
 }
